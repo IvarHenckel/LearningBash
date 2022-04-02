eaf24a5f697da41f360094a9aad429f2f0d84eb0
IvarHenckel <henckelivar@gmail.com>
2022-04-02 23:28:25 +0200

    Cleaned comments and terminal messages in InteractiveGitPush.sh

--- ScriptExamples/InteractiveGitPush.sh
+++ ScriptExamples/InteractiveGitPush.sh
@@ -10,9 +10,10 @@ ANY_ADD=0
 for FILE in "$(git status | grep "modified")"
 do
     # Remove everything up to and including ":" and xargs removes spaces
-    FILE=${FILE#*:} | xargs
+    FILE=${FILE#*: }
+    FILE=$(echo $FILE | xargs)
     while true; do
-        read -p "Add file ? (yes, no, quit): " answer
+        read -p "Add file $FILE? (yes, no, quit): " answer
         if [ "$answer" == "quit" ]; then
             exit 0
         elif [ "$answer" == "yes" ]; then
@@ -31,33 +32,3 @@ if [ $ANY_ADD -eq 1 ]; then
     read -p "About to commit and push. Insert message: " COM_MSG
     git commit -m "$COM_MSG" && git push
 fi
-
-
-# FILES=("senap")
-# git status | grep "modified" | while read line
-# do
-#     # echo $line
-#     # echo "johnyys ${FILES[@]}"
-#     # FILES+=("${line#*: }")
-#     FILES+=("gurka")
-#     echo "${FILES[@]} hej"
-# done
-
-# echo "${FILES[@]} hej"
-
-# for FILE in "${FILES[@]}"
-# do
-#     echo "$FILE"
-# done
-
-# echo "Add ${line#*: } ?"
-# while true
-# do
-# read addAnswer
-# #if [ "$addAnswer" -eq "quit" ]
-# #then
-# #    exit
-# #else
-#     echo "Please write \"quit\", \"yes\" or \"no\""
-# #fi
-# done
